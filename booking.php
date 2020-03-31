cab9887e764bd819501d2e70ae35be6f96c5e19a
Stefan2468 <44094044+Stefan2468@users.noreply.github.com>
2020-03-31 11:11:23 +0100

    Hotmail email

--- booking.php
+++ booking.php
@@ -5,17 +5,19 @@
 <head>
 	<title>Self Catering Apartments Richmond UK</title>
 	<link href="css_cascading_style_script/styles.css" rel="stylesheet" type="text/css" media="screen">
+	<script type="text/javascript" src="salsa_calendar/SalsaCalendar.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="salsa_calendar/SalsaCalendar.min.css" media="all" />
     <script src="java_script/jquery_js.js"></script>
 	<script src="java_script/jquery_ui.js"></script>
 	<script>
-	
+
 	var $cpn = Math.floor(Math.random()*9000)+1000;
-	
+
 	$( document ).ready(function() {
-		
+
 		$("#cpimg").attr('src', 'http://www.shipplets.com/dd_DynamicGraphic.php?text=' + $cpn);
 	});
-	
+
 	function captChk() {
     	if (document.forms["myForm"]["input_code"].value==$cpn) {
 			document.forms["myForm"]["code"].value = ($cpn*1111-1)*2-1;
@@ -27,9 +29,9 @@
 
 			return false;
     	}
-	}	
+	}
 	</script>
-	
+
 	<script>
   		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -40,31 +42,31 @@
   		ga('send', 'pageview');
 
 	</script>
-	
+
 </head>
-	
+
 <body>
 
 <div id="wrapper">
-	
+
 	<div id="top">
 		<div id ="background" style="background-image:url('images/index/river-banner.jpg');">
 		</div>
 			<img id ="door_logo_1" alt="Door logo" src="images/index/door-logo.png" />
-			
+
 			<h1>Larkfield Apartments</h1>
-			
+
 		<div id="topnav1">
-			
+
 			<ul>
 				<li><a href="index.html">HOME</a></li>
 				<li><a href="location.html">LOCAL ATTRACTIONS</a></li>
 				<li><a href="booking-information.html">BOOKING INFORMATION</a></li>
 				<li><a href="booking.php">CONTACT US</a></li>
-			
-			
+
+
 			</ul>
-			
+
 		</div>
 
 	</div>
@@ -84,88 +86,88 @@
 
 	</div>
 
-	
+
 	<div id="booking_page">
-			
+
 		<h1>Contact Information</h1>
 		<br>
-		
+
 		<p>You can contact us directly via the email address or telephone number provided below, or alternatively fill out our booking enquiry form.</p>
 		<br>
-		
+
 		<h2>Email:</h2>
 		<p><a href="mailto:shipplets@hotmail.com?subject=Accommodation%20enquiry" style="text-decoration: none;color: black;">shipplets@hotmail.com</a></p>
 		<br>
-				
+
 		<h2>Telephone Number:</h2>
 		<p>+44 (0) 208 948 6620</p>
 		<br>
-		
+
 		<h2>Correspondence Address:</h2>
 		<p>Larkfield Apartments,
 		<br> Ground Floor, 19 Larkfield Road,
-		<br> Richmond-Upon-Thames, Surrey, TW9 2PG		
+		<br> Richmond-Upon-Thames, Surrey, TW9 2PG
 		</p>
 		<br>
-					
-		
+
+
 	<br>
 	<h2 style="font-size:24px;">Booking Enquiry Form:</h2>
 	<br>
-	
+
 	</div>
-	
+
 	<div style="text-align:center; font-size:19px;">
 		<form action="grantsscript.php" name="myForm" method="post" id="booking_form" onsubmit="return captChk();">
-			
+
 			Contact Name <strong>(required)</strong><br>
 			<input type="text" name="name" class="input1"><br><br>
-	
+
 			Address<br>
 			<textarea name="address" rows="2" class="text_area1"></textarea>
 			<br><br>
-	
+
 			Telephone <strong>(required)</strong><br>
 			<input type="text" name="telephone" class="input1"><br><br>
-	
+
 			Email <strong>(required)</strong><br>
 			<input type="text" name="email" class="input1"><br><br>
 
 			Date of Arrival<br>
-			<input type="text" name="arrival" class="input1"><br><br>
-			
+			<input type="text" id="salsaCalendar_checkin" name="arrival" class="salsa-calendar-input form-control input1" autocomplete="off" style="background: url(salsa_calendar/calendarIco.png) no-repeat right 11px center;"><br><br>
+
 			Date of Departure<br>
-			<input type="text" name="departure" class="input1"><br><br>
-	
+			<input type="text" id="salsaCalendar_checkout" name="departure" class="salsa-calendar-input form-control input1" autocomplete="off" style="background: url(salsa_calendar/calendarIco.png) no-repeat right 11px center;"><br><br>
+
 			Number of People<br>
 			<input type="text" name="number_of_people" class="input1"><br><br>
-			
+
 			Will you Require Parking?<br>
 			<input type="text" name="parking" class="input1"><br><br>
-	
+
 			Enter any enquiries you may have in the box below<br>
 			<textarea name="enquiry" rows="8" class="text_area2"></textarea><br><br>
-	
+
 			<div style="margin:15px 0px 15px 0px;">
 				<img src="http://www.shipplets.com/dd_DynamicGraphic.php?text=0000" id="cpimg" alt="" />
 			</div>
 			<input name="code" type="hidden" value="0" />
 			<p><strong>Please enter the code above in the box below and then select submit.</strong></p>
-			
+
 			<input name="input_code" type="text" maxlength="4" class="input2" />
-			
+
 			<br><br>
-            
+
             <p style="color:red; display:none;" id="errorBox">
   				<strong>Please enter the code correctly and then click submit.</strong>
 			</p>
 			<input type="reset" value="reset" class="input_button">
 			<input type="submit" name="submit" value="submit" class="input_button">
-						
+
 		</form>
 	</div>
-	<br>	
-	
+	<br>
+
 	<div style="clear:both;">
 
 		<br><br><br><br><br><br><br>
@@ -184,11 +186,45 @@
 	</div>
 
 	<br><br>
-	
+
 
 </div>
 
+<script type="text/javascript">
+  var calendar_from = new SalsaCalendar({
+    inputId: 'salsaCalendar_checkin',
+    lang: 'en',
+    range: {
+      min: 'today'
+    },
+    calendarPosition: 'right',
+    fixed: false,
+    connectCalendar: true
+  });
+
+  var calendar_to = new SalsaCalendar({
+    inputId: 'salsaCalendar_checkout',
+    lang: 'en',
+    range: {
+      min: 'today'
+    },
+	calendarPosition: 'right',
+    fixed: false
+  });
+
+  // new SalsaCalendar.Connector({
+  //   from: calendar_from,
+  //   to: calendar_to,
+  //   maximumInterval: 21,
+  //   minimumInterval: 1
+  // });
+  //
+  // new SalsaCalendar.NightsCalculator({
+  //   from: calendar_from,
+  //   to: calendar_to,
+  //   nightsNo: 'nights-no'
+  // });
+</script>
+
 </body>
-
-
 </html>
--- grantsscript.php
+++ grantsscript.php
@@ -1,12 +1,22 @@
 <?php
-// OPTIONS - PLEASE CONFIGURE THESE BEFORE USE!
 
-$yourEmail = "shipplets@hotmail.com"; // the email address you wish to receive these mails through
-$yourWebsite = "Shipplets.com - Enquiry"; // the name of your website
-$thanksPage = ''; // URL to 'thanks for sending mail' page; leave empty to keep message on the same page 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/* Exception class. */
+require 'PHPMailer/Exception.php';
+
+/* The main PHPMailer class. */
+require 'PHPMailer/PHPMailer.php';
+
+/* SMTP class, needed if you want to use SMTP. */
+require 'PHPMailer/SMTP.php';
+
+
+// PLEASE CONFIGURE THESE
+$thanksPage = ""; // URL to 'thanks for sending mail' page; leave empty to keep message on the same page
 $requiredFields = "name,email,telephone"; // names of the fields you'd like to be required as a minimum, separate each field with a comma
 
-// DO NOT EDIT BELOW HERE
 $error_msg = array();
 $result = null;
 
@@ -25,85 +35,110 @@ function isBot() {
 
 	if (empty($_SERVER['HTTP_USER_AGENT']) || $_SERVER['HTTP_USER_AGENT'] == " ")
 		return true;
-	
+
 	return false;
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	if (isBot() !== false)
-		$error_msg[] = "No bots please! UA reported as: ".$_SERVER['HTTP_USER_AGENT'];
-		
+		$error_msg[] = "No bots please. UA reported as: ".$_SERVER['HTTP_USER_AGENT'];
+
 	foreach($requiredFields as $field) {
 		trim($_POST[$field]);
-		
-		if (!isset($_POST[$field]) || empty($_POST[$field]) && array_pop($error_msg) != "Please click the 'Back' button of your browser and fill in the required fields\r\n")
-			$error_msg[] = "Please click the 'Back' button of your browser and fill in the required fields";
+
+		if (!isset($_POST[$field]) || empty($_POST[$field]) && array_pop($error_msg) != "Please click the 'Back' button on your browser and fill in the fields marked as 'required'.\r\n")
+			$error_msg[] = "Please click the 'Back' button on your browser and fill in the fields marked as 'required'.\r\n";
 	}
 
 /*	if (!empty($_POST['name']) && !preg_match("/^[a-zA-Z-'\s]*$/", stripslashes($_POST['name'])))
 		$error_msg[] = "The name field must not contain special characters.\r\n"; */
-		
+
 	if (!empty($_POST['email']) && !preg_match('/^([a-z0-9])(([-a-z0-9._])*([a-z0-9]))*\@([a-z0-9])(([a-z0-9-])*([a-z0-9]))+' . '(\.([a-z0-9])([-a-z0-9_-])?([a-z0-9])+)+$/i', strtolower($_POST['email'])))
-		$error_msg[] = "Please fill in a valid email address, press 'Back' to go back to the form.\r\n";
-		
+		$error_msg[] = "Please input a valid email address: click the 'Back' button on your browser to return to the enquiry form.\r\n";
+
 /*	if (!empty($_POST['url']) && !preg_match('/^(http|https):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i', $_POST['url']))
 		$error_msg[] = "Invalid website url.\r\n";*/
-	
+
 	$codeNumber_encoded = intval(clean($_POST['code']));
 	//echo intval(clean($_POST['input_code']))."\r\n";
 	$codeInput_as_typed_in = ((($codeNumber_encoded + 1)/2)+1)/1111;
 	//echo $codeInput_as_typed_in."\r\n";
-	if ($codeInput_as_typed_in != intval(clean($_POST['input_code']))) $error_msg = 'Your message was identified as spam and could not be sent. Please try again';
-	
+	if ($codeInput_as_typed_in != intval(clean($_POST['input_code']))) $error_msg = "Your enquiry was identified as spam and could not be sent. Please try again, or email your enquiry direct to shipplets@hotmail.com.\r\n";
+
 	if ($error_msg == NULL) {
-		$subject = "Automatic Form Email";
-		
-		$message = "You received this e-mail message through your website: \n\n";
-		
-		$count = count($_POST);
-		
-		foreach ($_POST as $key => $val) {
-		    if (--$count <= 2) {
-        		break;
-    		}
-			if (is_array($val)) {
-				foreach ($val as $subval) {
-					$message .= ucwords($key) . ": " . clean($subval) . "\r\n";
+
+		// Set up email
+
+		$mail = new PHPMailer(true); // Create a new PHPMailer object
+
+		try { //for error handling
+			$mail->setFrom('shippletsenquiries@outlook.com', 'www.shipplets.com enquiry form'); // Set the mail sender
+			$mail->addAddress('shipplets@hotmail.com'); // Add a recipient
+			$mail->AddReplyTo($_POST['email'], $_POST['name']);
+			$mail->Subject = 'Shipplets Enquiry Form Automated Email'; // Set the subject
+
+			$message = "This is an automated email from www.shipplets.com Booking Enquiry Form. The following was input into the form by the user: \n\n";
+
+			$count = count($_POST);
+
+			foreach ($_POST as $key => $val) {
+			    if (--$count <= 2) {
+	        		break;
+	    		}
+				if (is_array($val)) {
+					foreach ($val as $subval) {
+						$message .= ucwords($key) . ": " . clean($subval) . "\r\n";
+					}
+				} else {
+					$message .= ucwords($key) . ": " . clean($val) . "\r\n";
 				}
-			} else {
-				$message .= ucwords($key) . ": " . clean($val) . "\r\n";
 			}
-		}
-		$message .= "\r\n";
-		$message .= 'IP: '.$_SERVER['REMOTE_ADDR']."\r\n";
-		$message .= 'Browser: '.$_SERVER['HTTP_USER_AGENT']."\r\n";
+			$message .= "\r\n";
+			$message .= 'IP: '.$_SERVER['REMOTE_ADDR']."\r\n";
+			$message .= 'Browser: '.$_SERVER['HTTP_USER_AGENT']."\r\n";
 
-		if (strstr($_SERVER['SERVER_SOFTWARE'], "Win")) {
-			$headers   = "From: $yourEmail\r\n";
-		} else {
-			$headers   = "From: $yourWebsite <$yourEmail>\r\n";	
-		}
-		$headers  .= "Reply-To: {$_POST['email']}\r\n";
+			$mail->Body = $message; // Set the mail message body
 
-		if (mail($yourEmail,$subject,$message,$headers)) {
-			
-			$error_msg[] = "Thank you for your submission.<br>
-			Please click the 'Back' button of your browser to return to the website";
-			//if (!empty($thanksPage)) {
-			//	header("Location: $thanksPage");
-			//	exit;
-			//} else {
-			//	$result = 'Your mail was successfully sent.';
-			//	$disable = true;
-			//}
-		} else {
-			$error_msg[] = 'Your mail could not be sent.';
+			// SMTP parameters
+
+			$mail->isSMTP(); // Tells PHPMailer to use SMTP
+			$mail->Host = 'smtp-mail.outlook.com'; // SMTP server address
+			$mail->SMTPAuth = TRUE; // Use SMTP authentication
+			$mail->SMTPSecure = 'tls'; // Set the encryption system
+			$mail->Username = 'shippletsenquiries@outlook.com'; // SMTP authentication username
+			$mail->Password = 'Grapes1089'; // SMTP authentication password
+			$mail->Port = 587; // Set the SMTP port
+
+			// Finally produce error message or send email
+
+			if(!$mail->send()) {
+				$error_msg[] = "Unfortunately, your enquiry could not be sent. Please try again, or email your enquiry direct to shipplets@hotmail.com.\r\n";
+			} else {
+				$error_msg[] = "Thank you for your enquiry- we will aim to reply to you within the next few hours, subject to the time zone difference.<br>
+				Please click the 'Back' button on your browser to return to the Shipplets website.\r\n";
+				//if (!empty($thanksPage)) {
+				//	header("Location: $thanksPage");
+				//	exit;
+				//} else {
+				//	$result = 'Your mail was successfully sent.';
+				//	$disable = true;
+				//}
+			}
+		}catch (phpmailerException $e) {
+			$error_msg[] = "Unfortunately, your enquiry could not be sent. Please try again, or email your enquiry direct to shipplets@hotmail.com. <br> <br>";
+
+		  	$error_msg[] = $e->errorMessage(); //Pretty error messages from PHPMailer
+		} catch (Exception $e) {
+			$error_msg[] = "Unfortunately, your enquiry could not be sent. Please try again, or email your enquiry direct to shipplets@hotmail.com. <br> <br>";
+
+		  	$error_msg[] = $e->getMessage(); //Boring error messages from anything else!
 		}
+
 	} else {
 		if (empty($error_msg))
-			$error_msg[] = 'Your message was identified as spam and could not be sent. Please try again';
+			$error_msg[] = "Your enquiry was identified as spam and could not be sent. Please try again, or email your enquiry direct to shipplets@hotmail.com.\r\n";
 	}
-	
+
 }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html>
